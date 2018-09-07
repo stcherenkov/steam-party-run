bc145e930c7b15d4b4c508cc1055d5ec58abea93
Stanislav Cherenkov <dev@stnslv.ch>
2018-09-07 10:44:50 +0300

    Setup frontend environment

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/front/index.jsx',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'static/app')
+  },
+  mode: process.env.NODE_ENV,
+  module: {
+    rules: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: [
+            require('@babel/preset-env'),
+            require('@babel/preset-react')
+          ],
+          plugins: [
+            '@babel/plugin-syntax-class-properties',
+            'transform-class-properties'
+          ]
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            query: {
+              modules: true,
+              camelCase: true,
+              localIdentName: process.env.NODE_ENV === 'production' ?
+                '[hash:base64:12]':
+                '[path][name]__[local]--[hash:base64:5]'
+            }
+          }
+        ],
+      }
+    ]
+  }
+}
